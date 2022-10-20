a1b920ff1fe24b7089cdf3704a0187c8f6693273
ROY <113372901+APPHIK@users.noreply.github.com>
2022-10-20 09:23:51 +0700

    Update config.py

--- Ingram/utils/config.py
+++ Ingram/utils/config.py
@@ -9,9 +9,9 @@ class Config:
     def __init__(self):
         self.TIMEOUT = 3  # (default) will be reset in the run_ingram.py
         self.USERS = ['admin']  # user names for Brute force cracking of weak passwords
-        self.PASSWDS = ['abcd1234', 'Abcd1234', 'Admin123', 'admin123', 'hd543211', 'Admin12345', 'admin12345', 'Hd543211', 'admin123456', 'Admin123456', 'abc123abc', 'Abcd12345', 'abcd12345', 'admin', 'asdf1234', 'abc12345', '12345admin', '12345abc', 'abc123abc', '123abc123']
+        self.PASSWDS = ['abcd1234', 'Abcd1234', 'Admin123', 'admin123', 'hd543211', 'Admin12345', 'admin12345', 'Hd543211', 'admin123456', 'Admin123456', '123456', 'abc123abc', 'Abcd12345', 'abcd12345', 'asdf1234', 'abc12345', '12345admin', '12345abc', 'abc123abc', '123abc123']
         self.USERAGENT = get_user_agent()  # to save time, we only get user agent once.
-        self.PORT = [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 180, 280, 38, 480, 580, 8000, 8001, 8002, 8003, 8004, 8005, 8008, 8080, 8081, 8085, 8086, 8088, 8181, 9000, 37777, 34599, 34582]
+        self.PORT = [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 180, 280, 380, 480, 580, 680, 780, 880, 980, 1080, 8888, 8000, 8001, 8002, 8003, 8004, 8005, 8008, 8080, 8081, 8085, 8086, 8088, 8181, 9000, 37777, 34599, 34582]
 
         # device names
         self.NON_MATCH_DEV = 'other'
